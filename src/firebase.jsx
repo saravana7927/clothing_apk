0733a52bd053bd817f058750c557ed1c9f55c07f
saravana perumal s <78338304+saravana7927@users.noreply.github.com>
2024-10-03 20:16:37 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,13 +4,9 @@ import {getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import "firebase/compat/firestore";
 
-firebase.initializeApp({  apiKey: "AIzaSyDDrc6HraOb3MiA5y4o6squuzBRzEaxKeo",
-  authDomain: "light-spot-mens-wear.firebaseapp.com",
-  projectId: "light-spot-mens-wear",
-  storageBucket: "light-spot-mens-wear.appspot.com",
-  messagingSenderId: "365046866216",
-  appId: "1:365046866216:web:86613c10c6958ff7d6f01d",
-  measurementId: "G-1QJYWMP95E"});
+firebase.initializeApp({
+  //enter firebase config
+});
 
 const firebaseConfig = {
  //enter firebase config
